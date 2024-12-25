da5ae5177ac7cd8d2871a93cc15335cd7d471230
Enol Casielles <enolcasielles@gmail.com>
2024-12-25 11:27:23 +0100

    implements delete saved list method

--- src/modules/lists/controllers/delete-saved-list.controller.ts
+++ src/modules/lists/controllers/delete-saved-list.controller.ts
@@ -1,14 +1,18 @@
-import { NextApiRequest } from "next";
-import { deleteSavedListService } from "../services/delete-saved-list.service";
 import { CustomError } from "@/core/errors";
 import { apiError } from "@/core/api-responses/api-error";
+import { deleteSavedListService } from "../services/delete-saved-list.service";
+import { Role } from "@/core/enums/role.enum";
+import { authenticate } from "@/core/lib/auth";
 import { NextResponse } from "next/server";
 import { successResponse } from "@/core/responses/basic.response";
 
-export async function DeleteSavedListController(request: NextApiRequest) {
+export async function DeleteSavedListController(
+  request: Request,
+  { params }: { params: { listId: string } },
+) {
   try {
-    const { listId } = request.query;
-    await deleteSavedListService({ listId: listId.toString() });
+    const userId = await authenticate(request, Role.USER);
+    await deleteSavedListService(params.listId, userId);
     return NextResponse.json(successResponse());
   } catch (error) {
     if (error instanceof CustomError) {
@@ -17,7 +21,7 @@ export async function DeleteSavedListController(request: NextApiRequest) {
     return apiError(
       new CustomError({
         message:
-          "Se ha producido un error inesperado intentando eliminar la lista guardada",
+          "Se ha producido un error inesperado al eliminar la lista guardada",
       }),
     );
   }
--- src/modules/lists/services/delete-saved-list.service.ts
+++ src/modules/lists/services/delete-saved-list.service.ts
@@ -1,8 +1,40 @@
-interface Props {
-  listId: string;
-}
+import { prisma } from "@/core/lib/prisma";
+import { CustomError } from "@/core/errors";
 
-export async function deleteSavedListService({ listId }: Props) {
-  // TODO: Implementar eliminar lista guardada
-  console.log(listId);
+export async function deleteSavedListService(
+  listId: string,
+  userId: string,
+): Promise<void> {
+  const user = await prisma.user.findUnique({
+    where: {
+      id: userId,
+    },
+    include: {
+      savedLists: {
+        where: {
+          id: listId,
+        },
+      },
+    },
+  });
+
+  if (!user.savedLists.length) {
+    throw new CustomError({
+      message: "La lista no está guardada",
+      statusCode: 404,
+    });
+  }
+
+  await prisma.user.update({
+    where: {
+      id: userId,
+    },
+    data: {
+      savedLists: {
+        disconnect: {
+          id: listId,
+        },
+      },
+    },
+  });
 }
